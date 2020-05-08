546191b835b074b27ffa3064119a21bef354d740
nosahama <emmanuelnosa.e@gmail.com>
2020-05-08 04:25:05 +0200

    Add index page.
    This page renders the root path of our app.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,30 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
+import Layout from "../layouts"
 import SEO from "../components/seo"
+
+// const IndexPage = () => (
+//   <Layout>
+//     <SEO title="Home" />
+//     <h1>COVID-19 Tracker</h1>
+//     <p>Get updates and visually appealing representations of the current pandemic across multiple countries.</p>
+//     <Link to="/page-2/">Go to page 2</Link>
+//   </Layout>
+// )
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div>
+      <div className="Hero">
+        <div className="HeroGroup">
+          <h1>COVID-19 Tracker</h1>
+          <p>Get updates and visually appealing representations of the current pandemic across multiple countries.</p>
+          <Link to="/page-2/">Global Statuses</Link>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
