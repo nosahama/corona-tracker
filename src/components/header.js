5999ca0a9a1059a8500ef09f0f05ab6671a13d5b
nosahama <emmanuelnosa.e@gmail.com>
2020-05-08 07:31:16 +0200

    Add header scroll animation and stateful component

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,50 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
 import './header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className="Header">
-    <div className="HeaderGroup">
-      <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
-      <Link to="/countries">Countries</Link>
-      <Link to="/updates">Updates</Link>
-      <Link to="/forecast">Forecast</Link>
-      <Link to="/forecast"><button>Live</button></Link>
-    </div>
-  </div>
-)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
 
-Header.defaultProps = {
-  siteTitle: ``,
+    // Add initial state
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  // Listen for events and pass the callback function
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  // Event listener callback
+  handleScroll = (event) => {
+    console.log(event)
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({hasScrolled: true})
+    }
+    else {
+      this.setState({hasScrolled: false})
+    }
+  }
+
+  render() {
+    {/** Add a css class based on some property state */}
+    return (
+      <div className={this.state.hasScrolled ? 'HeaderScrolled Header' : 'Header'}>
+        <div className="HeaderGroup">
+          <Link to="/"><img src={require('../images/logo-designcode.svg')} width="30" /></Link>
+          <Link to="/countries">Countries</Link>
+          <Link to="/updates">Updates</Link>
+          <Link to="/forecast">Forecast</Link>
+          <Link to="/forecast"><button>Live</button></Link>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Header
